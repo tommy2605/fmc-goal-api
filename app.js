107d761e640cf89306efb13c3107f7cd0cd8cd08
LAPTOP-C284JHA0\hufer <huferry@gmail.com>
2020-04-14 06:36:01 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const goals = require('./goals')
+
+const app = express()
+
+app.listen(22000, () => console.log('listening..'));
+
+app.get('/app/goals/', async (req, res) => {
+    try {
+        const result = await goals.find(req.query)
+        res.json(result)
+    } catch (err) {
+        console.err(err)
+        res.sendStatus(500)
+    }    
+})
+
+app.get('/app/goals/:date', async (req, res) => {
+
+    console.log(req.params)
+    const query = req.query || {}
+    if (req.params.date) query.date = req.params.date
+
+    try {
+        const result = await goals.find(query)
+        res.json(result)
+    } catch (err) {
+        console.err(err)
+        res.sendStatus(500)
+    }
+    
+})
+
---
+++ goals.js
@@ -0,0 +1,53 @@
+const fs = require('fs').promises
+const path = require('path')
+
+
+const find = async (query) => {
+    const { culture, date, after } = query || {}
+
+    const files = await fs.readdir('./data')
+    const goalsPromises = files
+        .map(fn => path.join('./data', fn))
+        .map(readGoal)
+
+    const goals = await Promise.all(goalsPromises)
+
+    const queryDate = date ? parseDate(date) : undefined
+    const afterDate = after ? parseDate(after) : undefined
+
+    return goals
+        .filter(goal => !culture || goal.culture === culture.toLowerCase())
+        .filter(goal => !queryDate || goal.publishDate.valueOf() === queryDate.valueOf())
+        .filter(goal => !afterDate || goal.publishDate.valueOf() > afterDate.valueOf())
+}
+
+const readGoal = async (fileName) => {
+    const content = (await fs.readFile(fileName)).toString()
+    const lineBreak = content.indexOf('\n')
+    return {
+        publishDate: parseDate(fileName),
+        culture: parseCulture(fileName),
+        title: content.substr(0, lineBreak).trim(),
+        content: content.substr(lineBreak + 1).replace(/\r\n/g, '')    
+    }
+}
+
+const parseDate = (fileName) => {
+    const parsedDate = fileName.match(/(\d{4})(\d{2})(\d{2})/i)
+    if (parsedDate) {
+        return new Date(
+            parsedDate[1], 
+            parseInt(parsedDate[2]) - 1, 
+            parseInt(parsedDate[3]) + 1)
+    }
+    return new Date()
+}
+
+const parseCulture = (fileName) => {
+    const parsedCulture = fileName.match(/\d{8}_([a-z]{2})/i)
+    return parsedCulture ? parsedCulture[1].toLowerCase() : undefined
+}
+
+module.exports = {
+    find
+}
