1d013d6803af35e354cff2ddf77a68c02e1a4c19
LAPTOP-C284JHA0\hufer <huferry@gmail.com>
2020-04-23 19:07:04 +0200

    refactor

--- frontend/src/components/GoalEditor.js
+++ frontend/src/components/GoalEditor.js
@@ -2,37 +2,47 @@ import React, { useState } from 'react'
 import TextEditor from './TextEditor'
 import './GoalEditor.css'
 
-const InputText = ({ key, label }) => {
+const InputText = ({ key, label, value, onChange }) => {
   return (
     <div className="form-group row">
       <label for={key} className="col-sm-3 col-form-label">{`${label} :`}</label>
       <div className="col sm-8">
-        <input className="form-control" id={key} />
+        <input 
+          className="form-control" 
+          id={key}
+          value={value}
+          onChange={e => onChange(e.target.value)} />
       </div>
     </div>
   )
 }
+const OneDayInMSecs = 24 * 3600 * 1000
+const OneWeekInMSecs = 7 * OneDayInMSecs
 
-const InputDate = ({ key, label }) => {
-
-  const OneDayInMSecs = 24 * 3600 * 1000
-  const OneWeekInMSecs = 7 * OneDayInMSecs
+const nextSunday = (iter) => {
 
   const getToday = () => {
     const today = new Date().valueOf()
     const time = today % OneDayInMSecs
     return new Date(today - time)
-  }
+  }  
 
-  const nextSunday = (iter) => {
-    if (!iter) return nextSunday(getToday())
-    return iter.getDay() === 0
-      ? iter
-      : nextSunday(new Date(iter.valueOf() + OneDayInMSecs))
-  }
+  if (!iter) return nextSunday(getToday())
+  return iter.getDay() === 0
+    ? iter
+    : nextSunday(new Date(iter.valueOf() + OneDayInMSecs))
+}
+
+const InputDate = ({ key, label, value, onChange }) => {
+
 
   const getSunday = (weeksFromNow) =>
     new Date(nextSunday().valueOf() + weeksFromNow * OneWeekInMSecs)
+
+  const isSelected = (date, idx) => {
+    if (date) return date.valueOf() === nextSunday().valueOf()
+    return idx === 4
+  }
 
   const dates = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     .map((_, idx) => {
@@ -40,23 +50,26 @@ const InputDate = ({ key, label }) => {
       return (
         <option
           key={idx}
-          selected={idx === 4}
+          selected={isSelected(date, idx)}
         >{date.toDateString()}</option>
       )
     })
-
+  
   return (
     <div className="form-group row">
       <label for={key} className="col-sm-3 col-form-label">{`${label} :`}</label>
       <div className="col sm-8">
-        <select id={key} className="form-control">
+        <select 
+          id={key} 
+          className="form-control"
+          value={value}
+          onChange={e => onChange(new Date(e.target.value))} >
           {dates}
         </select>
       </div>
     </div>
   )
 }
-
 
 const InputChoice = ({ key, label, choices }) => {
   return (
@@ -79,11 +92,24 @@ const InputChoice = ({ key, label, choices }) => {
 
 const GoalEditor = (props) => {
 
+  const [title, setTitle] = useState()
+  const [date, setDate] = useState()
+
   return (
     <div className="GoalEditor">
+      <div>{date && date.toString()}</div>
       <div className="topNavigation">
-        <InputText key="title" label="Title" />
-        <InputDate key="date" label="Date" />
+        <InputText 
+          key="title" 
+          label="Title" 
+          value={title}
+          onChange={value => setTitle(value)}/>
+
+        <InputDate 
+          key="date" 
+          label="Date"
+          value={date}
+          onChange={value => setDate(value)} />
       </div>
       
       <TextEditor
