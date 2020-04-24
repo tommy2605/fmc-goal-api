3b6a27f8f6a48ec63453a9781c0c30d0d78f6448
Ferry Utomo <huferry@gmail.com>
2020-04-24 07:58:14 +0200

    Add language guessing

--- frontend/src/components/GoalEditor.js
+++ frontend/src/components/GoalEditor.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import franc from 'franc'
 import TextEditor from './TextEditor'
 import InputText from './InputText'
 import InputDate, { nextSunday } from './InputDate'
@@ -11,6 +12,7 @@ const GoalEditor = (props) => {
   const [title, setTitle] = useState()
   const [date, setDate] = useState(nextSunday().valueOf())
   const [content, setContent] = useState()
+  const [lang, setLang] = useState()
 
   const canPublish = () => {
     return title && title.length > 5
@@ -34,14 +36,21 @@ const GoalEditor = (props) => {
       </div>
       
       <TextEditor
-        onChange={html => setContent(html)}
+        onChange={html => {
+          setContent(html)
+          const text = html.replace(/<\/?[a-z0-9]+>/g,' ')
+          const lang = franc(text)
+          if (['ind', 'nld', 'eng'].includes(lang)) setLang(lang)
+        }}
       />
 
       <div className="bottomNavigation">
         <InputChoice
           key="language"
           label="Language"
-          choices={["Indonesia", "English", "Nederlands"]} />
+          value={lang}
+          onChange={newLang => setLang(newLang)}
+          choices={["ind/Indonesia", "eng/English", "nld/Nederlands"]} />
         <button
           className="btn btn-success btn-sm"
           disabled={!canPublish()}
--- frontend/src/components/InputChoice.js
+++ frontend/src/components/InputChoice.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
-const InputChoice = ({ id, label, choices }) => {
+const InputChoice = ({ id, label, value, choices, onChange }) => {
   return (
     <div className="form-group row">
       <label htmlFor={id} className="col-sm-3 col-form-label">{`${label} :`}</label>
       <div className="col sm-8">
-        <select id={id} className="form-control">
+        <select 
+          id={id} 
+          className="form-control" 
+          value={value}
+          onChange={e => onChange(e.target.value)} >
           {
             choices.map(item => {
+              const keyText = item.match(/(.+)\/(.+)/)
+              const key = keyText ? keyText[1] : item
+              const text = keyText ? keyText[2] : item
               return (
-                <option key={item}>{item}</option>
+                <option key={key} value={key}>{text}</option>
               )
             })
           }
